567ea2707457349f773bded61273fe5ad804f848
Nick Spicer <nick.spicer@mountainwarehouse.com>
2019-12-15 15:53:19 +0000

    more work on recursive table

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,51 @@ type AppState = {
     hiddenGroupings: string[]
 }
 
+const recursiveGroup = (rows: Data[], groupKeys: string[], groupKeyIndex: number, columns: ActionProperty[]): any[] => {
+    if (groupKeyIndex === groupKeys.length - 1) return rows;
 
+    return _.chain(rows).groupBy(groupKeys[groupKeyIndex]).map((groupedRows, groupKey) => {
+        const row = {};
+        for (const prop of columns) {
+            row[prop.property] = aggregateSwitch(prop.action, groupedRows, prop.property, groupKeys[groupKeyIndex], groupKey);
+            row["datas"] = recursiveGroup(groupedRows, groupKeys, groupKeyIndex + 1, columns);
+        }
+        return row;
+    }).value();
+}
+
+type ActionProperty = {
+    property: string;
+    action: ActionType;
+}
+
+const columns: ActionProperty[] = [
+    { property: "category", action: "GROUP" },
+    { property: "department", action: "GROUP" },
+    { property: "type", action: "GROUP" },
+    { property: "ref", action: "GROUP" },
+    { property: "colour", action: "GROUP" },
+    { property: "sno", action: "GROUP" },
+    { property: "week", action: "GROUP" },
+    { property: "value", action: "SUM" },
+];
+
+type ActionType = "GROUP" | "HIDE" | "SUM" | "DISTINCT" | "AVERAGE" | "COUNT";
+
+const aggregateSwitch = (action: ActionType, rows: Data[], property: string, currentGroupingProperty: string, currentGroupingValue: string) => {
+    switch (action) {
+        case "SUM":
+            return _.sumBy(rows, x => x[property]);
+        case "DISTINCT":
+            return _.uniqBy(rows, x => x[property]).map(x => x[property]).join(", ");
+        case "AVERAGE":
+            return _.meanBy(rows, x => x[property]);
+        case "GROUP":
+            return currentGroupingProperty === property ? currentGroupingValue : null;
+        case "HIDE":
+            return null;
+    }
+}
 
 export default class App extends React.Component<{}, AppState> {
     state = {
@@ -17,164 +61,14 @@ export default class App extends React.Component<{}, AppState> {
     render() {
         const { data2, hiddenGroupings } = this.state;
 
-        // We have an ordered list of attributes we want to do something with, and what we should do with the values.
-        // columns
-        const columns = [
-            { property: "category", action: "group", order: 1, },
-            { property: "department", action: "sum", order: 2, },
-            { property: "type", action: "group", order: 3, },
-            { property: "ref", action: "group", order: 4, },
-            { property: "colour", action: "hide", order: 5, },
-            { property: "sno", action: "hide", order: 6, },
-            { property: "warehouse", action: "hide", order: 7, },
-            { property: "branchGrade", action: "hide", order: 8, },
-            { property: "branch", action: "hide", order: 9, },
-            { property: "week", action: "group", order: 10, },
-            { property: "value", action: "sum", order: 11, }
-        ];
-
         // Can decide which columns to group by and the order
         // Then decide which columns to aggregate by, and how
-
-
-
-        const groups = ["category", "department", "type", "ref", "colour", "sno", "warehouse", "branchGrade", "branch", "week"];
-        const groupedValues = {};
-        for (const row of data2) {
-            // 
-            for (const group of groups) {
-                groupedValues.push({
-
-                })
-            }
-        }
-        const aggregates = [
-            { property: "value", action: (array, x) => array.reduce() },
-            { property: "sno", action: x => }
-        ];
-        const groupings = [];
-        // group by all, each time we group do something with the values
-        for (const group of groups) {
-            const result = _.chain(data2).groupBy(group).map((data, grouping) => { }).value();
-            groupings.push(result);
-        }
-        // foreach item in the array, group by item, add some values;
-        let groupedArray = [{}];
-        for (var group of groups) {
-            for (var row of groupedArray) {
-                groupedArray = _.chain(data2).groupBy(group).map((data, grouping) => ({
-                    category: "",
-                    value: _.sumBy(data, x => x.value),
-                    key: 
-                })).value();
-            }
-        }
-        const grouped = data2.reduce((result, data) => {
-            groups.reduce((group, key, i, { length }) => {
-                // rolling value, current item, current index, length of array
-                return group[data[key]] = group[data[key]] || (i + 1 === length ? [] : {})
-            }, result).push(data);
-            return result;
-        }, {});
-
-        console.log(grouped);
-
-        const test: Data[] = [];
-        // Function takes groupings array, index, until empty populate all values with next value?
-        // Populate all value types
-        for (const group of groupings) {
-            _.chain(data2).groupBy(group).map((datas, groupProperty) => ({
-
-            }));
-        }
-        const catGroup: Data[] = _.chain(data2).groupBy(x => x.category).map((datas, category) => ({
-            id: null,
-            category: category,
-            department: "",
-            type: "",
-            ref: "",
-            colour: "",
-            sno: "",
-            warehouse: "",
-            branchGrade: "",
-            branch: "",
-            week: null,
-            value: _.sumBy(datas, x => x.value),
-            ids: _.map(datas, x => x.id),
-            datas: _.chain(datas).groupBy(x => x.department).map((depRows, department) => ({
-                id: null,
-                category: "",
-                department: department,
-                type: "",
-                ref: "",
-                colour: "",
-                sno: "",
-                warehouse: "",
-                branchGrade: "",
-                branch: "",
-                week: null,
-                value: _.sumBy(depRows, x => x.value),
-                ids: _.map(depRows, x => x.id),
-                datas: _.chain(datas).groupBy(x => x.type).map((typeRows, type) => ({
-                    id: null,
-                    category: "",
-                    department: "",
-                    type: type,
-                    ref: "",
-                    colour: "",
-                    sno: "",
-                    warehouse: "",
-                    branchGrade: "",
-                    branch: "",
-                    week: null,
-                    value: _.sumBy(typeRows, x => x.value),
-                    ids: _.map(typeRows, x => x.id),
-                })).value()
-            })).value()
-        })).value();
-
-        const deptGroup: Data[] = _.chain(data2).groupBy(x => `${x.category}-${x.sno}`).map((datas, group) => ({
-            id: datas[0].id,
-            category: datas[0].category,
-            department: datas[0].department,
-            type: "",
-            ref: "",
-            colour: "",
-            sno: datas[0].sno,
-            warehouse: "",
-            branchGrade: "",
-            branch: "",
-            week: null,
-            value: _.sumBy(datas, x => x.value),
-            ids: _.map(datas, x => x.id),
-            datas: datas
-        })).value();
-
-        const weekGroup: Data[] = _.chain(data2).groupBy(x => `${x.category}-${x.week}`).map((datas, group) => ({
-            id: datas[0].id,
-            category: datas[0].category,
-            department: datas[0].department,
-            type: "",
-            ref: "",
-            colour: "",
-            sno: null,
-            warehouse: "",
-            branchGrade: "",
-            branch: "",
-            week: datas[0].week,
-            value: _.sumBy(datas, x => x.value),
-            ids: _.map(datas, x => x.id),
-            datas: datas
-        })).value();
-
+        const groups = columns.filter(x => x.action === "GROUP").map(x => x.property);
+        const groupedRows = recursiveGroup(data2, groups, 0, columns);
         return (
             <React.Fragment>
                 <div className="container-fluid">
-                    {nestedTable("category", catGroup, this.updateValue, data2, this.toggleRow, hiddenGroupings)}
-                    {table(catGroup, this.updateValue, data2)}
-                    {table(deptGroup, this.updateValue, data2)}
-                    {table(weekGroup, this.updateValue, data2)}
-                    {table(data2, this.updateValue, data2)}
+                    {nestedTable("category", groupedRows, this.updateValue, data2, this.toggleRow, hiddenGroupings, columns)}
                 </div>
             </React.Fragment>
         )
@@ -202,18 +96,10 @@ export default class App extends React.Component<{}, AppState> {
     }
 }
 
-const nestedRow = (groupingKey: string, rows: Data[], updateValue: Function, allRows: Data[], toggleRow: Function, hiddenGroups: string[]) =>
-    rows.map(x => <React.Fragment><tr key={x.id} onClick={() => toggleRow(groupingKey)} style={{ backgroundColor: x.datas ? "lightgrey" : "white" }}>
-        <td>{x.category}</td>
-        <td>{x.department}</td>
-        <td>{x.type}</td>
-        <td>{x.ref}</td>
-        <td>{x.colour}</td>
-        <td>{x.sno}</td>
-        <td>{x.warehouse}</td>
-        <td>{x.branchGrade}</td>
-        <td>{x.branch}</td>
-        <td>{x.week}</td>
+const nestedRow = (groupingKey: string, rows: Data[], updateValue: Function, allRows: Data[], toggleRow: Function, hiddenGroups: string[], columns: ActionProperty[]) =>
+    rows.map(x => <React.Fragment><tr key={x.id} style={{ backgroundColor: x.datas ? "lightgrey" : "white" }}>
+        {columns.map(y => <td onClick={() => toggleRow(groupingKey)}>{x[y.property]}</td>)}
+        {/* 
         <td suppressContentEditableWarning={true} contentEditable={true} onBlur={(e) => {
             const oldValue = x.value;
             const newValue = Math.round(parseInt(e.currentTarget.innerText));
@@ -229,80 +115,21 @@ const nestedRow = (groupingKey: string, rows: Data[], updateValue: Function, all
                 // get new value, 
                 updateValue([x]);
             }
-        }}>{Math.round(x.value)}</td>
+        }}>{Math.round(x.value)}</td> */}
     </tr>
-        {x.datas && !hiddenGroups.some(y => y === groupingKey) && nestedRow(groupingKey, x.datas, updateValue, allRows, toggleRow, hiddenGroups)}
+        {x.datas && !hiddenGroups.some(y => y === groupingKey) && nestedRow(groupingKey, x.datas, updateValue, allRows, toggleRow, hiddenGroups, columns)}
     </React.Fragment>
     );
 
-const nestedTable = (grouping: string, rows: Data[], updateValue: Function, allRows: Data[], toggleRow: Function, hiddenGroupings: string[]) =>
+const nestedTable = (grouping: string, rows: Data[], updateValue: Function, allRows: Data[], toggleRow: Function, hiddenGroupings: string[], columns: ActionProperty[]) =>
     <table className="table table-sm collapse show">
         <thead>
             <tr>
-                <th>Category</th>
-                <th>Department</th>
-                <th>Type</th>
-                <th>Ref</th>
-                <th>Colour</th>
-                <th>Sno</th>
-                <th>Warehouse</th>
-                <th>BranchGrade</th>
-                <th>Branch</th>
-                <th>Week</th>
-                <th>Value</th>
+                {columns.map(x => <th>{x.property}</th>)}
             </tr>
         </thead>
         <tbody>
-            {nestedRow(grouping, rows, updateValue, allRows, toggleRow, hiddenGroupings)}
-        </tbody>
-    </table>
-
-const table = (rows: Data[], updateValue: Function, allRows: Data[]) =>
-    <table className="table table-sm collapse show">
-        <thead>
-            <tr>
-                <th>Category</th>
-                <th>Department</th>
-                <th>Type</th>
-                <th>Ref</th>
-                <th>Colour</th>
-                <th>Sno</th>
-                <th>Warehouse</th>
-                <th>BranchGrade</th>
-                <th>Branch</th>
-                <th>Week</th>
-                <th>Value</th>
-            </tr>
-        </thead>
-        <tbody>
-            {rows.map(x => <tr key={x.id}>
-                <td>{x.category}</td>
-                <td>{x.department}</td>
-                <td>{x.type}</td>
-                <td>{x.ref}</td>
-                <td>{x.colour}</td>
-                <td>{x.sno}</td>
-                <td>{x.warehouse}</td>
-                <td>{x.branchGrade}</td>
-                <td>{x.branch}</td>
-                <td>{x.week}</td>
-                <td suppressContentEditableWarning={true} contentEditable={true} onBlur={(e) => {
-                    const oldValue = x.value;
-                    const newValue = Math.round(parseInt(e.currentTarget.innerText));
-
-                    if (x.ids && x.ids.length > 0) {
-                        const children = allRows.filter(x1 => x.ids && x.ids.some(y => y === x1.id));
-                        for (let child of children) {
-                            child.value = Math.round((child.value / oldValue) * newValue);
-                        }
-                        updateValue(children);
-                    } else {
-                        x.value = newValue;
-                        // get new value, 
-                        updateValue([x]);
-                    }
-                }}>{Math.round(x.value)}</td>
-            </tr>)}
+            {nestedRow(grouping, rows, updateValue, allRows, toggleRow, hiddenGroupings, columns)}
         </tbody>
     </table>
 
